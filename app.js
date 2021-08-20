2d29aa33df8a0d296f2a1650f50eca27bb58a84b
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-20 12:35:55 +0400

    get data from api

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=f6d03af80fa5dae802d2c03c9582ec83&query=37.8267,-122.4233';
 
 request({url: url, json: true}, (error, response) => {
-    // const data = JSON.parse(response.body)
-    // console.log(data.current);
-    console.log(response.body.current);
+    const temprature = response.body.current.temperature;
+    const feelsLike = response.body.current.feelslike;
+    console.log(`It is currently ${temprature} degree out, it feels ${feelsLike} degree out.`);
 })
