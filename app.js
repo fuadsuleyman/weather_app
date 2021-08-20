4879b8c21def4a8c99ac15d2cb7182bc197a04ec
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-20 11:56:23 +0400

    json:true optinon of request object, not need JSON.parse

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const request = require('postman-request');
 
 const url = 'http://api.weatherstack.com/current?access_key=f6d03af80fa5dae802d2c03c9582ec83&query=37.8267,-122.4233';
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current);
+request({url: url, json: true}, (error, response) => {
+    // const data = JSON.parse(response.body)
+    // console.log(data.current);
+    console.log(response.body.current);
 })
