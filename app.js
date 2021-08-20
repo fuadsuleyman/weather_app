d2e6122e3b8fa5e18eea3019ab7b6dbbc4ea1b76
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-20 12:55:03 +0400

    change api data as in documantation with adding query to url

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const request = require('postman-request');
-
+// https://weatherstack.com/documentation
+// const url = 'http://api.weatherstack.com/current?access_key=f6d03af80fa5dae802d2c03c9582ec83&query=37.8267,-122.4233&units=f';
+// const url = 'http://api.weatherstack.com/current?access_key=f6d03af80fa5dae802d2c03c9582ec83&query=37.8267,-122.4233&units=s';
 const url = 'http://api.weatherstack.com/current?access_key=f6d03af80fa5dae802d2c03c9582ec83&query=37.8267,-122.4233';
 
 request({url: url, json: true}, (error, response) => {
