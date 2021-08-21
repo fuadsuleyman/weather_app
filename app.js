501d28fa541b82f91c5e95f6f4ef38eba3e17d7d
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-08-21 19:00:32 +0400

    callback chaining get latute and long and give to getForecast function

--- app.js
+++ app.js
@@ -7,27 +7,32 @@ const address = 'Baku';
 
 getGeoCode(address, (error, data) => {
     if (error){
-        console.log(chalk.error(error));
-    } else {
-        console.log(data)
-        return data;
-        // console.log(chalk.success(`${data.place_name} latut is ${data.latitude}, longtut is ${data.longitude}`));
-    }
+        return console.log(chalk.error(error));
+    } 
+    getForecast(data, (error, forecastData) => {
+        if(error){
+            return console.log(chalk.error(error));
+        }
+        console.log(chalk.success(data.place_name));
+        console.log(chalk.success(`${forecastData.weather_descriptions} .It is currently ${forecastData.temprature} degree out, it feels ${forecastData.feelsLike} degree out.`));
+    })
+    // console.log(data);
+    // return data;
 })
 
 // test data for getForecast function
-const geoCode = {
-    latitude: 40.36666,
-    longitude: 49.83518
-}
+// const geoCode = {
+//     latitude: 40.36666,
+//     longitude: 49.83518
+// }
 
-getForecast(geoCode, (error, data) => {
-    if(error){
-        console.log(chalk.error(error));
-    } else {
-        console.log(data);
-    }
-})
+// getForecast(geoCode, (error, data) => {
+//     if(error){
+//         console.log(chalk.error(error));
+//     } else {
+//         console.log(data);
+//     }
+// })
 
 
 
